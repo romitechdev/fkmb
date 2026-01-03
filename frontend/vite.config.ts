9f21337e65076bea80b15afa1a845807827281d0
Romi <rominmuh230@gmail.com>
2026-01-03 07:10:37 +0700

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://zany-hedy-romi-project-7e326665.koyeb.app',
         changeOrigin: true,
       },
     },
